9a8b1369954a4456146c8f1476d162abd9447c36
moewob <afdlfzn@gmail.com>
2024-06-16 04:37:04 +0000

    garnet: Remove unwanted packages

--- device.mk
+++ device.mk
@@ -504,6 +504,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackagesGarnet
+
 # Servicetracker
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.servicetracker@1.2.vendor
---
+++ remove_packages/Android.mk
@@ -0,0 +1,29 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackagesGarnet
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+    Accord \
+    Chrome \
+    Chrome-Stub \
+    ConnMO \
+    Drive \
+    DCMO \
+    FilesPrebuilt  \
+    Gramophone \
+    Maestro \
+    Maps \
+    RecorderPrebuilt \
+    SafetyHubPrebuilt \
+    Symphonica \
+    Tycho \
+    VoiceAccessPrebuilt \
+    VZWAPNLib \
+    YouTube
+
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
